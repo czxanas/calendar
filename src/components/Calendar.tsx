c61297f8df1cb6e6b9f9796eb934096845fa0119
czxanas <anas.essahib1998@gmail.com>
2024-11-05 17:03:11 +0100

    Install date-fns, let's test it

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,8 +1,14 @@
+import { format } from "date-fns"
 
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
+    const result = format(new Date(), 'dd/MM/yyyy')
+    return (
+        <div>
+            <span className="font-semibold">Calendar:</span>
+            &nbsp;
+            <span>{result}</span>
+        </div>
+    )
 }
 
 export default Calendar
