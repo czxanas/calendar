e771015189d49ec45e7df22f1a4ebfb3f25d7b06
czxanas <anas.essahib1998@gmail.com>
2024-11-06 01:35:46 +0100

    Change the calendar style to light

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,21 +3,22 @@ import { daysOfEachMonthType } from "../types/daysOfEachMonthType"
 import { getDaysOfYear } from "../utils/getDaysOfYear"
 
 const Calendar = () => {
-    const months: daysOfEachMonthType = getDaysOfYear(2024)
+    const year: number = 2024
+    const months: daysOfEachMonthType = getDaysOfYear(year)
     return (
-        <div className="pb-8 border px-4 rounded-lg h-[500px] overflow-y-scroll overflow-x-hidden">
+        <div className="pb-8 border px-4 rounded-lg h-[500px] w-[300px] overflow-y-scroll overflow-x-hidden bg-white text-black">
             {
                 Object.entries(months).map(([month, days]) => (
                     <div key={month}>
-                        <h2 className="font-semibold mt-5 mb-1 text-center">{month}</h2>
+                        <h2 className="font-semibold my-5 text-center">{month} {year}</h2>
                         <div className="grid grid-cols-7 gap-1">
                             {
-                                daysNames.map((day) => <div key={day} className="italic">{day}</div>)
+                                daysNames.map((day) => <div key={day} className="font-thin text-sm text-center mb-2">{day}</div>)
                             }
                         </div>
                         <ul className="grid grid-cols-7 gap-1">
                             {days.map((day, index) => (
-                                <li className="border rounded py-0.5 px-1 cursor-pointer" key={index}>
+                                <li className="rounded py-0.5 px-1 cursor-pointer text-center" key={index}>
                                     {day || ' '}
                                 </li>
                             ))}
